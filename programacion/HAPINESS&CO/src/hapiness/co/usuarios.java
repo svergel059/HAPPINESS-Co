d81c199fa5196ff80861adad380de95f88bdb392
sebas <svergel059@gmail.com>
2026-03-26 12:41:12 +0100

    creacion del proyecto y de la clase usuario

---
+++ programacion/HAPINESS&CO/src/hapiness/co/usuarios.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hapiness.co;
+
+/**
+ *
+ * @author ALUMNOS-FP
+ */
+
+/*Atributo*/
+public class usuarios {
+    
+    private String nombre;
+    private String email;
+    private String password;
+
+
+    public usuarios(String nombre, String email, String password) {
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+   
+    public String toString() {
+    return "Usuario [nombre=" + nombre + ", email=" + email + "]";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
