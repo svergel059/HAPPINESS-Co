8edc98b09d6712b307c3550fe81c703a5b72e66f
sebas <svergel059@gmail.com>
2026-03-27 10:54:13 +0100

    modificación metodos getter y setter

--- programacion/HAPINESS&CO/src/hapiness/co/Galeria.java
+++ programacion/HAPINESS&CO/src/hapiness/co/Galeria.java
@@ -9,22 +9,53 @@ package hapiness.co;
  *
  * @author ALUMNOS-FP
  */
-
 public class Galeria {
-    
+
     private static int contadorId = 0;
 
     private int id;
-    
+
     private String titulo;
 
     private int idEvento;
 
- 
     public Galeria(String titulo, int idEvento) {
         this.id = contadorId; // Asignamos el ID actual al objeto
         contadorId++;  // Incrementamos el contador para el siguiente objeto
-        this.titulo   = titulo;
+        this.titulo = titulo;
         this.idEvento = idEvento;
     }
+
+    public static int getContadorId() {
+        return contadorId;
+    }
+
+    public static void setContadorId(int contadorId) {
+        Galeria.contadorId = contadorId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getIdEvento() {
+        return idEvento;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setIdEvento(int idEvento) {
+        this.idEvento = idEvento;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
