52e16adc6397baedad4c7c5d1465064972a15b16
sebas <svergel059@gmail.com>
2026-03-27 10:39:59 +0100

    se crea el constructor de la clase Galeria y los metodos getter y setter

--- programacion/HAPINESS&CO/src/hapiness/co/Galeria.java
+++ programacion/HAPINESS&CO/src/hapiness/co/Galeria.java
@@ -9,6 +9,22 @@ package hapiness.co;
  *
  * @author ALUMNOS-FP
  */
+
 public class Galeria {
     
+    private static int contadorId = 0;
+
+    private int id;
+    
+    private String titulo;
+
+    private int idEvento;
+
+ 
+    public Galeria(String titulo, int idEvento) {
+        this.id = contadorId; // Asignamos el ID actual al objeto
+        contadorId++;  // Incrementamos el contador para el siguiente objeto
+        this.titulo   = titulo;
+        this.idEvento = idEvento;
+    }
 }
