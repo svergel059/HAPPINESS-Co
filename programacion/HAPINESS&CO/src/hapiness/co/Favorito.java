f2414f86c00f51ca2b27ce8964f2cc30faf71537
sebas <svergel059@gmail.com>
2026-03-27 12:24:57 +0100

    se crea el constructor de la clase Favoritos y los metodos getter y setter

--- programacion/HAPINESS&CO/src/hapiness/co/Favorito.java
+++ programacion/HAPINESS&CO/src/hapiness/co/Favorito.java
@@ -10,5 +10,14 @@ package hapiness.co;
  * @author ALUMNOS-FP
  */
 public class Favorito {
-    
+
+    private String correoUsuario;
+
+    private int idEvento;
+
+    public Favorito(String correoUsuario, int idEvento) {
+        this.correoUsuario = correoUsuario; // Guardamos el email del usuario
+        this.idEvento = idEvento;      // Guardamos el ID del evento
+    }
+
 }
